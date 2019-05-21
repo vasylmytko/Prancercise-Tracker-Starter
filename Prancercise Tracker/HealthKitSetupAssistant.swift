ece2aa9af65a0a34836d57d9d40b5a970a856f2e
vmytko <mytkovasyl97@gmail.com>
2019-05-21 13:58:49 +0300

    Authorizing HealthKit

--- Prancercise Tracker/HealthKitSetupAssistant.swift
+++ Prancercise Tracker/HealthKitSetupAssistant.swift
@@ -38,6 +38,38 @@ class HealthKitSetupAssistant {
   }
   
   class func authorizeHealthKit(completion: @escaping (Bool, Error?) -> Swift.Void) {
+    guard HKHealthStore.isHealthDataAvailable() else {
+        completion(false, HealthkitSetupError.notAvailableOnDevice)
+        return
+    }
     
+    guard   let dateOfBirth = HKObjectType.characteristicType(forIdentifier: .dateOfBirth),
+        let bloodType = HKObjectType.characteristicType(forIdentifier: .bloodType),
+        let biologicalSex = HKObjectType.characteristicType(forIdentifier: .biologicalSex),
+        let bodyMassIndex = HKObjectType.quantityType(forIdentifier: .bodyMassIndex),
+        let height = HKObjectType.quantityType(forIdentifier: .height),
+        let bodyMass = HKObjectType.quantityType(forIdentifier: .bodyMass),
+        let activeEnergy = HKObjectType.quantityType(forIdentifier: .activeEnergyBurned) else {
+            
+            completion(false, HealthkitSetupError.dataTypeNotAvailable)
+            return
+    }
+    
+    let healthKitTypesToWrite: Set<HKSampleType> = [bodyMassIndex,
+                                                    activeEnergy,
+                                                    HKObjectType.workoutType()]
+    
+    let healthKitTypesToRead: Set<HKObjectType> = [dateOfBirth,
+                                                   bloodType,
+                                                   biologicalSex,
+                                                   bodyMassIndex,
+                                                   height,
+                                                   bodyMass,
+                                                   HKObjectType.workoutType()]
+    
+    HKHealthStore().requestAuthorization(toShare: healthKitTypesToWrite,
+                                         read: healthKitTypesToRead) { (success, error) in
+                                            completion(success, error)
+    }
   }
 }
